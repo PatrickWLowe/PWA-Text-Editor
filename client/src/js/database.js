c9bf9cbbd5824f6c0f0601154b792da32e300be7
Pat Lowe <lowe-patrick@att.net>
2023-08-29 17:48:34 -0700

    added script to install.js and database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,20 +13,22 @@ const initdb = async () =>
   });
 
 export const putDb = async (content) => {
-  const db = await openDB("jate", 1);
-  const tx = db.transaction("jate", "readwrite");
-  const store = tx.objectStore("jate");
-  await store.put(content);
-  await tx.done;
+  const JDB = await openDB("jate", 1);
+  const text = JDB.transaction("jate", "readwrite");
+  const store = text.objectStore("jate");
+  const req = store.put({ id: 1, value: content });
+  const result = await req;
+  console.log(result);
 };
 
 export const getDb = async () => {
-  const db = await openDB("jate", 1);
-  const tx = db.transaction("jate", "readonly");
-  const store = tx.objectStore("jate");
-  const content = await store.getAll();
-  await tx.done;
-  return content;
+  console.log("GET from the database");
+  const JDB = await openDB("jate", 1);
+  const text = JDB.transaction("jate", "readonly");
+  const store = text.objectStore("jate");
+  const req = store.getAll();
+  const result = await req;
+  return result?.value;
 };
 
 initdb();
